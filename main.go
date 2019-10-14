1883099b3405b063db030e3c81c95c42f161cdfe
deathrizzo <methods@hotmail.com>
2019-10-13 21:25:59 -0700

    working with actions

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	name := query.Get("name")
 	if name == "" {
-		name = "Guest"
+		name = "guest"
 	}
 	log.Printf("Received request for %s\n", name)
 	w.Write([]byte(fmt.Sprintf("Hello, %s\n", name)))
